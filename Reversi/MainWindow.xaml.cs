2aac8de7aa96c16e072c539041d75dff6f32e77c
Ishay Muchtar <ishay3000@gmail.com>
2021-01-28 22:27:17 +0200

    Added classes representing the game board and game tiles.

--- Reversi/MainWindow.xaml.cs
+++ Reversi/MainWindow.xaml.cs
@@ -20,6 +20,50 @@ namespace Reversi
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        class Tile
+        {
+            public Brush Brush { get; set; }
+            public string Tag { get; set; }
+
+            public Tile(Brush brush, string tag)
+            {
+                Brush = brush;
+                Tag = tag;
+            }
+
+            public Tile()
+            {
+                Brush = Brushes.Transparent;
+                Tag = "";
+            }
+        }
+        class Board
+        {
+            public List<Tile> GameTiles;
+            private readonly int boardSize;
+
+            public Board(int boardSize)
+            {
+                this.boardSize = boardSize;
+                GameTiles = new List<Tile>();
+                InitializeTiles();
+            }
+
+            private void InitializeTiles()
+            {
+                for (int i = 0; i < boardSize; i++)
+                {
+                    for (int j = 0; j < boardSize; j++)
+                    {
+                        GameTiles.Add(new Tile());
+                    }
+                }
+
+                GameTiles[Convert.ToInt32(Math.Pow(boardSize, 2) / 2)].Brush = Brushes.Red;
+                //GameTiles[boardSize / 2 + 1].Brush = Brushes.Blue;
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -27,13 +71,9 @@ namespace Reversi
             var list = new List<int>();
 
             int boardSize = 6;
+            Board board = new Board(boardSize);
 
-            for (int i = 0; i < boardSize; i++)
-            {
-                list.AddRange(Enumerable.Range(i * 10, boardSize));
-            }
-
-            DataContext = list;
+            DataContext = board.GameTiles;
         }
     }
 }
